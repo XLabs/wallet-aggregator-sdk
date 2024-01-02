62723cbac9aa151f58b2307d16ebc92dae556c3a
Martin Picco <martin@xlabs.xyz>
2024-01-02 11:39:09 -0300

    Upgrade dependencies for wallet-aggregator-evm package (#18)
    * EVM: upgrade wagmi and add viem as dependency. All: upgrade typescript dependency
    
    * bump wagmi and viem dependencies, comment out Ledger wallet  class

--- packages/wallets/evm/src/chains.ts
+++ packages/wallets/evm/src/chains.ts
@@ -3,20 +3,17 @@ import { Chain } from "@wagmi/core/chains";
 
 export * from "@wagmi/core/chains";
 
-const karuraTestnet = {
-  id: 596,
-  name: "Karura Testnet",
-  network: "karura-testnet",
-  nativeCurrency: { name: "Karura Token", symbol: "KAR", decimals: 18 },
+const acala = {
+  id: 787,
+  name: "Acala",
+  network: "acala",
+  nativeCurrency: { name: "Acala Token", symbol: "ACA", decimals: 18 },
   rpcUrls: {
-    default: { http: ["https://karura-dev.aca-dev.network/eth/http"] },
-    public: { http: ["https://karura-dev.aca-dev.network/eth/http"] },
+    default: { http: ["https://eth-rpc-acala.aca-api.network"] },
+    public: { http: ["https://eth-rpc-acala.aca-api.network"] },
   },
   blockExplorers: {
-    default: {
-      name: "Blockscout",
-      url: "https://blockscout.karura-dev.aca-dev.network",
-    },
+    default: { name: "Blockscout", url: "https://blockscout.acala.network" },
   },
 } as const satisfies Chain;
 
@@ -51,31 +48,34 @@ const karura = {
   },
 } as const satisfies Chain;
 
-const acala = {
-  id: 787,
-  name: "Acala",
-  network: "acala",
-  nativeCurrency: { name: "Acala Token", symbol: "ACA", decimals: 18 },
+const karuraTestnet = {
+  id: 596,
+  name: "Karura Testnet",
+  network: "karura-testnet",
+  nativeCurrency: { name: "Karura Token", symbol: "KAR", decimals: 18 },
   rpcUrls: {
-    default: { http: ["https://eth-rpc-acala.aca-api.network"] },
-    public: { http: ["https://eth-rpc-acala.aca-api.network"] },
+    default: { http: ["https://karura-dev.aca-dev.network/eth/http"] },
+    public: { http: ["https://karura-dev.aca-dev.network/eth/http"] },
   },
   blockExplorers: {
-    default: { name: "Blockscout", url: "https://blockscout.acala.network" },
+    default: {
+      name: "Blockscout",
+      url: "https://blockscout.karura-dev.aca-dev.network",
+    },
   },
 } as const satisfies Chain;
 
-export const klaytnBaobab = {
-  id: 1001,
-  name: "Klaytn Testnet Baobab",
-  network: "klaytn-baobab",
-  nativeCurrency: { name: "Klay", symbol: "KLAY", decimals: 18 },
+export const emerald = {
+  id: 42262,
+  name: "Emerald Paratime Mainnet",
+  network: "emerald-testnet",
+  nativeCurrency: { name: "Emerald Rose", symbol: "ROSE", decimals: 18 },
   rpcUrls: {
-    default: { http: ["https://api.baobab.klaytn.net:8651"] },
-    public: { http: ["https://api.baobab.klaytn.net:8651"] },
+    default: { http: ["https://emerald.oasis.dev"] },
+    public: { http: ["https://emerald.oasis.dev"] },
   },
   blockExplorers: {
-    default: { name: "KlaytnScope", url: "https://baobab.scope.klaytn.com" },
+    default: { name: "Oasis", url: "https://explorer.emerald.oasis.dev" },
   },
 } as const satisfies Chain;
 
@@ -96,26 +96,12 @@ export const emeraldTestnet = {
   },
 } as const satisfies Chain;
 
-export const emerald = {
-  id: 42262,
-  name: "Emerald Paratime Mainnet",
-  network: "emerald-testnet",
-  nativeCurrency: { name: "Emerald Rose", symbol: "ROSE", decimals: 18 },
-  rpcUrls: {
-    default: { http: ["https://emerald.oasis.dev"] },
-    public: { http: ["https://emerald.oasis.dev"] },
-  },
-  blockExplorers: {
-    default: { name: "Oasis", url: "https://explorer.emerald.oasis.dev" },
-  },
-} as const satisfies Chain;
-
-export const DEFAULT_CHAINS: Chain[] = Object.values(CHAINS).concat([
+export const DEFAULT_CHAINS: Chain[] = [
+  ...Object.values(CHAINS),
   acala,
   acalaTestnet,
   emerald,
   emeraldTestnet,
   karura,
   karuraTestnet,
-  klaytnBaobab,
-]);
+];
